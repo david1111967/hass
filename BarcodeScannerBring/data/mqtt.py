0ee9a43cdb86fbf85eee6ccc16cc3611793c2961
david1111967 <david1111967@gmail.com>
2024-04-14 20:54:56 +0200

    Bug client mqtt

--- BarcodeScannerBring/data/mqtt.py
+++ BarcodeScannerBring/data/mqtt.py
@@ -16,21 +16,17 @@ class mqttBarScanner:
         client.subscribe(topic='lector_codigo_barras/out', qos=2)
 
     def on_message(self, client, userdata, message):
-        print(str(message.payload, encoding='utf-8'))
-        Bring = BringExtract(str(message.payload, encoding='utf-8'), self.userBring, self.passBring)
-        result = Bring.search()
-        if (result == None):
-            client.publish("lector_codigo_barras/in", "Error 404")
-        else:
-            client.publish("lector_codigo_barras/in", "" + result)
+        if (str(message.payload, encoding='utf-8') != "" or str(message.payload, encoding='utf-8') != None):
+            print(str(message.payload, encoding='utf-8'))
+            Bring = BringExtract(str(message.payload, encoding='utf-8'), self.userBring, self.passBring)
+            result = Bring.search()
+            if (result == None):
+                client.publish("lector_codigo_barras/in", "Error 404")
+            else:
+                client.publish("lector_codigo_barras/in", "" + result)
+            client.publish("lector_codigo_barras/out")
 
     def main(self):
-        print(self.userMqtt)
-        print(self.passMqtt)
-        print(self.ipMqtt)
-        print(self.portMqtt)
-        print(self.userBring)
-        print(self.passBring)
         client = paho.mqtt.client.Client(paho.mqtt.client.CallbackAPIVersion.VERSION1, client_id='API-Codigo_barras', clean_session=False)
         client.username_pw_set(username=self.userMqtt, password=self.passMqtt)
         client.connect(host=self.ipMqtt, port=int(self.portMqtt))
